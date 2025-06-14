36cee82a3326f8dbfafbd731286de7b66ae4d038
parka9296 <parka9296@student.faytechcc.edu>
2025-06-14 12:39:00 -0400

    Add files via upload

---
+++ P1LAB1_ParkAshlee.py
@@ -0,0 +1,10 @@
+# Ashlee Park
+# 6/13/2025
+# P1LAB1
+# Program that allows user to input their name and then displays a welcome message
+
+print("Enter your first name:", end=" ")
+first_name = input()
+print("Enter your last name:", end=" ")
+last_name = input()
+print("Hello,", first_name, last_name, "! Welcome to CTI-110")
