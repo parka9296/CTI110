fa19b2050ca0a7f6bbeb85dacdd0c2c4e5d15dc8
parka9296 <parka9296@student.faytechcc.edu>
2025-07-18 17:57:47 -0400

    Add files via upload
    program calculate measurements of a circle

---
+++ P2LAB1_ParkAshlee.py
@@ -0,0 +1,21 @@
+# Ashlee Park
+# 6/17/25
+# P2LAB1
+# Program to calculate measurements of a circle
+
+
+import math 
+r=8.75
+p=math.pi
+d=2*r
+c=2*p*r
+a=p*r**2
+
+
+print("What is the radius of the circle?", 8.75, "\n")
+
+print(f"The diameter of the circle is {d:.1f}", "\n")
+
+print(f"The circumference of the circle is {c:.2f}", "\n")
+
+print(f"The area of the circle is {a:.3f}")
