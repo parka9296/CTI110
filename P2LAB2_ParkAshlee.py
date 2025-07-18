4f369af6548f1fad38eb8290fbb7ca3973ad8b65
parka9296 <parka9296@student.faytechcc.edu>
2025-07-18 17:58:45 -0400

    Add files via upload
    program for various car mileage

---
+++ P2LAB2_ParkAshlee.py
@@ -0,0 +1,12 @@
+#Ashlee Park
+#6/18/25
+#P2LAB2
+#Program for various car mileage
+
+
+cars=(["Camero":18.21, "Prius":52.36, "Model S":110, "Silverado":26])
+
+print("Enter vehicle to see mileage:", end=" ")
+vehicle_mileage = input()
+
+print(vehicle_mileage[key_name])
